e36889098e4fddb7433c92120f55dfb2fc11739a
Gustavo <gordono@unsa.edu.pe>
2022-03-04 18:15:41 -0500

    Add functionality was built

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,6 @@
 package Calculator;
 class Calculator {
-    int add(int a, int b) { return 0; }
+    int add(int a, int b) { return a + b; }
     int sub(int a, int b) { return 0; }
     int mul(int a, int b) { return 0; }
     int div(int a, int b) { return 0; }
